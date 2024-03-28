906cdec777ac5d48fd624b8beba95c3a4d466cf4
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-03-28 18:01:21 +0200

    👥 feat: Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
-import Login from './pages/login/Login';
+// import Login from './pages/login/Login';
 // import Register from './pages/register/Register';
+import Home from './pages/home/Home';
 
 
 function App() {
 
   return (
     <div className=' p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Home/>
+      {/* <Login /> */}
       {/* <Register/> */}
     </div>
   );
