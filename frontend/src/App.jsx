c99e95864d92bd3efe5c5c26da949f750451000f
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-03-28 05:15:13 +0200

    👥 style: login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Login from './pages/login/Login';
+// import Register from './pages/register/Register';
+
 
 function App() {
 
   return (
     <div className=' p-4 h-screen flex items-center justify-center'>
-      <Login/>
+      <Login />
+      {/* <Register/> */}
     </div>
   );
 }
